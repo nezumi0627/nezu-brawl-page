b572411884aaf6d85ef28806a4cc245c0a1515ff
nezumi0627 <ren1108.km@gmail.com>
2024-10-31 19:19:34 +0900

    Allow-Origin

--- functions/getStats.js
+++ functions/getStats.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
-    const playerTag = 'PQ0J8VJUY';  // 固定のプレイヤーIDを設定
+    const playerTag = 'PQ0J8VJUY';  // 固定のプレイヤーID
     const apiUrl = `https://api.brawlstars.com/v1/players/%23${playerTag}`;
     const apiKey = process.env.BRAWL_API_KEY;
 
@@ -18,11 +18,19 @@ exports.handler = async function (event, context) {
         const data = await response.json();
         return {
             statusCode: 200,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'  // すべてのオリジンからのアクセスを許可
+            },
             body: JSON.stringify(data)
         };
     } catch (error) {
         return {
             statusCode: 500,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
             body: JSON.stringify({ error: 'Failed to fetch data', details: error.message })
         };
     }
