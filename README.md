de0b7eb038b50f8abcf2543964da783dc4c8af04
nezumi0627 <ren1108.km@gmail.com>
2024-10-31 19:36:04 +0900

    Fixxx

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # nezu-brawl-page
 Brawl Stars公式 apiを利用した詳細取得ページ
-<!-- 
--- functions/getStats.js
+++ functions/getStats.js
@@ -1,9 +1,11 @@
 async function fetchStats() {
-    const apiUrl = 'https://api.brawlstars.com/v1/players/%23PQ0J8VJUY'; // 固定のプレイヤーID
+    const playerId = 'PQ0J8VJUY'; // 固定のプレイヤーID
+    const apiUrl = `https://api.brawlstars.com/v1/players/%23${playerId}`; // Brawl Stars APIのURL
     const apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImY3ODJlOWFlLWUyZDAtNDI4MS05MjU4LTFjYzNlZTgyMDc0YyIsImlhdCI6MTczMDM2ODY5Niwic3ViIjoiZGV2ZWxvcGVyLzVmNWQ0NDQxLTk1ZWMtMTgxZi1kOGE1LWQ1MDAwOGMyNjRkNSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiNTkuMTcwLjE4My45OSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.eVH0cxm4BraTjvS1KMRbMp89POxZHRiv6GrA6O43YZciBNuuOv-T5VGreENNmChlHOEGlHWAy6sRTocrlo9Aag';  // Brawl Stars APIキー
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // CORS ProxyのURL
 
     try {
-        const response = await fetch(apiUrl, {
+        const response = await fetch(proxyUrl + apiUrl, {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${apiKey}`
